dabca3952ca81b9b02b5379b2afd943ff17f6b3e
Or Forte <or.forte1@gmail.com>
2021-10-21 11:58:36 +0300

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { BehaviorSubject, Subject } from 'rxjs';
+import { Post } from './post/post.model';
+import { PostService } from './post/post.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.less']
 })
-export class AppComponent {
-  title = 'mizrahi-test';
+export class AppComponent implements OnInit {
+  posts: BehaviorSubject<Post[]> = new BehaviorSubject<Post[]>([]);
+  isLoading: boolean = false;
+  currentPage: number = 1;
+
+  constructor(private postService: PostService) {
+  }
+
+  ngOnInit(): void {
+    this.isLoading = true;
+    this.postService.getPost(this.currentPage).subscribe(p => {
+      this.posts.next(p);
+      this.isLoading = false;
+    });
+  }
+
+  onScroll() {
+    this.isLoading = true;
+    this.postService.getPost(this.currentPage).subscribe(p => {
+      this.posts.next([... this.posts.value, ...p]);
+      this.isLoading = false;
+      this.currentPage++;
+    });
+  }
 }
